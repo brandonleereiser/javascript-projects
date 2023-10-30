028de387367b4f76eae44172a26e4b1b9399d7f6
Brandon Reiser <reiserb58@gmail.com>
2023-10-30 14:09:57 -0500

    exercises done;

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,22 @@
 function init () {
     const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
+    const button = document.body.querySelector('[data-target="#collapseExample"]');
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', function() { 
+        if(button.innerText.toLowerCase() === 'less') {
+            button.innerText = 'Take off';
+          }
+          else {
+            button.innerText = 'Houston! We have liftoff!';
+          }
+        });  
+ missionAbort.addEventListener("mouseover", function( event ) {
+event.target.style.backgroundColor = "red";
+});
+ missionAbort.addEventListener("mouseout", function( event ) {
+event.target.style.backgroundColor = "";
+});
 }
 
 window.addEventListener("load", init);
